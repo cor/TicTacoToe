d5aaafe218c9307a87fd4b0385528f30abfb71bc
Cor Pruijs <cor@pruys.com>
2014-02-25 23:23:28 +0100

    Added the Game class

---
+++ src/com/pruys/cor/Game.java
@@ -0,0 +1,26 @@
+package com.pruys.cor;
+
+/**
+ * Created by cor on 25-02-14.
+ */
+public class Game {
+
+    private String gameStatus = "TUTORIAL";
+
+
+
+
+   public Game () {
+
+
+       runGame();
+
+
+   }
+
+
+    private void runGame() {
+
+
+    }
+}
--- src/com/pruys/cor/Main.java
+++ src/com/pruys/cor/Main.java
@@ -2,7 +2,14 @@ package com.pruys.cor;
 
 public class Main {
 
+
     public static void main(String[] args) {
 	    System.out.println("Tic, Taco, Toe");
+        System.out.println("--------------");
+        System.out.println("Created by Cor Pruijs");
+
+        new Game();
     }
+
+
 }
