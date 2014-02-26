b972dcb7ecad4b695d1fef12cc7a9506bdc9a9fe
Cor Pruijs <cor@pruys.com>
2014-02-26 18:37:07 +0100

    Added an user input scan method Improved gameStatus handling fixed an bug that caused the program to crash when the user didn't give any input

--- src/com/pruys/cor/Game.java
+++ src/com/pruys/cor/Game.java
@@ -1,39 +1,48 @@
 package com.pruys.cor;
 
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 /**
  * Created by cor on 25-02-14.
  */
 public class Game {
 
-    private String gameStatus;
+    public String gameStatus = "STARTSCREEN";
 
 
 
 
     public Game () {
         clearScreen();
-        printIntro();
         runGame();
-
-        gameStatus = "TUTORIAL";
-
     }
 
 
     private void runGame() {
-        //TODO create main game loop
+
+
         while(!"SHUTDOWN".equals(gameStatus)){
 
 
             if("TUTORIAL".equals(gameStatus)) {
+
                 System.out.println("wow, much tutorial");
                 //TODO create tutorial
-                gameStatus = "SHUTDOWN";
+
+
+
             }
 
+            else if("STARTSCREEN".equals(gameStatus)) {
+                printIntro();
+                scanInput();
+                clearScreen();
 
+                gameStatus = "TUTORIAL";
+            }
 
-
+            else gameStatus = "SHUTDOWN";
         }
     }
 
@@ -66,10 +75,32 @@ public class Game {
         print("TicTacoToe");
         print("---------------------");
         print("Created by Cor Pruijs");
-
+        print("");
+        print("Press enter to begin");
     }
 
     private void print(String x) {
         System.out.println(x);
+    }
+
+    private String scanInput() {
+
+        String userInput;
+
+        try {
+
+        Scanner scanIn = new Scanner(System.in);
+        userInput = scanIn.nextLine();
+        scanIn.close();
+        return userInput;
+
+        }
+        catch ( java.util.NoSuchElementException x) {
+            //If the input is empty, return an empty string
+            return "";
+
+        }
+
+
     }
 }
