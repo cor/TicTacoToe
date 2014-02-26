cda075240103880939a53184572755c2625c9c62
Cor Pruijs <cor@pruys.com>
2014-02-26 17:28:35 +0100

    Added Game.class Added a simpliefid print method to safe some typing since this programe uses a lot of console printing Added a claer method that clears the screen

--- src/com/pruys/cor/Game.java
+++ src/com/pruys/cor/Game.java
@@ -5,22 +5,65 @@ package com.pruys.cor;
  */
 public class Game {
 
-    private String gameStatus = "TUTORIAL";
+    private String gameStatus;
 
 
 
 
-   public Game () {
+    public Game () {
+        clearScreen();
+        printIntro();
+        runGame();
 
+        gameStatus = "TUTORIAL";
 
-       runGame();
-
-
-   }
+    }
 
 
     private void runGame() {
+        //TODO create main game loop
+        while(!"SHUTDOWN".equals(gameStatus)){
 
 
+            if("TUTORIAL".equals(gameStatus)) {
+                System.out.println("wow, much tutorial");
+                //TODO create tutorial
+                gameStatus = "SHUTDOWN";
+            }
+
+
+
+
+        }
+    }
+
+
+    private void clearScreen(){
+
+        for(int i = 0; i < 100; i++) {
+            System.out.println("");
+        }
+    }
+
+    private void printIntro(){
+
+        /*
+        ┈┈┈┈╭╯╭╯╭╯┈┈┈┈┈
+        ┈┈┈╱▔▔▔▔▔╲▔╲┈┈┈
+        ┈┈╱┈╭╮┈╭╮┈╲╮╲┈┈
+        ┈┈▏┈▂▂▂▂▂┈▕╮▕┈┈
+        ┈┈▏┈╲▂▂▂╱┈▕╮▕┈┈
+        ┈┈╲▂▂▂▂▂▂▂▂╲╱┈┈
+         */
+
+
+        print("TicTacoToe");
+        print("---------------------");
+        print("Created by Cor Pruijs");
+
+    }
+
+    private void print(String x) {
+        System.out.println(x);
     }
 }
--- src/com/pruys/cor/Main.java
+++ src/com/pruys/cor/Main.java
@@ -4,9 +4,6 @@ public class Main {
 
 
     public static void main(String[] args) {
-	    System.out.println("Tic, Taco, Toe");
-        System.out.println("--------------");
-        System.out.println("Created by Cor Pruijs");
 
         new Game();
     }
