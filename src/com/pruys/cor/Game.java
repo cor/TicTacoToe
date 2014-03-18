3db344a4bfbf74f9b875a805f5f39c2dbebc8d29
Cor Pruijs <cor@pruys.com>
2014-03-18 21:06:24 +0100

    AI update
    Added AI
    Added a bunch of javadoc comments
    Added the option to quit the game at any time by typing quit
    Updated the startscreen screenshot to show the AI option

--- src/com/pruys/cor/Game.java
+++ src/com/pruys/cor/Game.java
@@ -1,5 +1,6 @@
 package com.pruys.cor;
 
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -10,14 +11,16 @@ import java.util.Scanner;
 public class Game {
 
     public String gameStatus = "STARTSCREEN";
-    private String lastUserInput;
+    private String lastUserInput = "init";
     private int[] level = new int[9];
+    public boolean aiEnabled = false;
 
     Scanner scanIn = new Scanner(System.in);
 
 
-
-
+    /**
+     * The only constructor, clears the screen and starts the main game loop
+     */
     public Game () {
         clearScreen();
         clearLevel();
@@ -25,15 +28,27 @@ public class Game {
     }
 
 
+
+    /**
+     * Runs the main game loop
+     */
     private void runGame() {
 
 
         while(!"SHUTDOWN".equals(gameStatus)){
 
-            if("STARTSCREEN".equals(gameStatus)) {
+            if ("quit".equals(lastUserInput.toLowerCase())) gameStatus = "SHUTDOWN";
+
+            else if("STARTSCREEN".equals(gameStatus)) {
+                aiEnabled = false;
                 clearScreen();
                 printIntro();
                 scanInput();
+
+                if("ai".equals(lastUserInput.toLowerCase())) {
+                    aiEnabled = true;
+                }
+
                 clearScreen();
                 clearLevel();
 
@@ -52,6 +67,7 @@ public class Game {
                 println("To choose a spot, you'll need to type the representative number and hit return");
                 println("Example: If one wants to choose the spot in the middle, he shall enter 5");
                 println("");
+                println("You can quit the game at any time by typing \"quit\" (without the quotes)");
                 println("Good Luck!             (Press enter to continue)" );
                 scanInput();
                 clearScreen();
@@ -70,6 +86,7 @@ public class Game {
 
                     if (gameWon(1)) gameStatus = "VICTORY_X";
                     else if (gameIsTie()) gameStatus = "TIE";
+                    else if (aiEnabled) gameStatus = "TURN_AI";
                     else gameStatus = "TURN_O";
                 }
                 else {
@@ -99,6 +116,20 @@ public class Game {
                 }
             }
 
+            else if("TURN_AI".equals(gameStatus)) {
+                int randomField = randomInt(1, 9);
+
+                if( fieldIsEmpty(randomField - 1)) {
+                    setField(2, randomField);
+                    clearScreen();
+                    println("AI changed field " + randomField + " to O");
+
+                    if(gameWon(2)) gameStatus = "VICTORY_AI";
+                    else if (gameIsTie()) gameStatus = "TIE";
+                    else gameStatus = "TURN_X";
+                }
+            }
+
             else if ("VICTORY_X".equals(gameStatus)) {
                 clearScreen();
                 printLevel();
@@ -119,6 +150,18 @@ public class Game {
                 gameStatus = "STARTSCREEN";
             }
 
+            else if ("VICTORY_AI".equals(gameStatus)) {
+                clearScreen();
+                printLevel();
+                println("THE AI HAS WON!");
+                println("You're probably extremely bad at this game...");
+                println("Press enter to play again");
+                scanInput();
+                clearScreen();
+                gameStatus = "STARTSCREEN";
+
+            }
+
             else if ("TIE".equals(gameStatus)) {
                 clearScreen();
                 printLevel();
@@ -133,7 +176,78 @@ public class Game {
         }
     }
 
-    private void printLevel(){
+    /**
+     *
+     * used by the printlevel class
+     *
+     * @param i the field you want to acces ( 0 - 8 )
+     * @return the character that needs to be placed here
+     */
+    private String getCharacter(int i) {
+
+        if( level[i] == 0) return("" + (i + 1));
+        else if( level[i] == 1) return("X");
+        else if( level[i] == 2) return("O");
+        else return("E");
+
+    }
+
+    /**
+     * Set a field in the level
+     *
+     * @param value 0 (empty), 1 (x) or 2 (o)
+     * @param position the field of the level you want to set.
+     */
+    private void setField(int value, int position) {
+        position--;
+        level[position] = value;
+    }
+
+    /**
+     * Resets level[]
+     */
+    private void clearLevel() {
+
+        for(int i = 0; i < 9; i++) {
+            level[i] = 0;
+        }
+
+    }
+
+    /**
+     * Returns a pseudo-random number between min and max, inclusive.
+     *
+     * @param min Minimum value
+     * @param max Maximum value.  Must be greater than min.
+     * @return Integer between min and max, inclusive.
+     * @see java.util.Random#nextInt(int)
+     */
+    public static int randomInt(int min, int max) {
+
+        Random rand = new Random();
+
+        // nextInt is normally exclusive of the top value,
+        // so add 1 to make it inclusive
+        int randomNum = rand.nextInt((max - min) + 1) + min;
+
+        return randomNum;
+    }
+
+
+
+
+    /**
+     * A shorter version of System.out.println() because this program uses a lot of console printing
+     * @param x the string to print
+     */
+    private void println(String x) {
+        System.out.println(x);
+    }
+
+    /**
+     * Prints the level in the console, accesses the level[] array for level data
+     */
+    private void printLevel() {
 
         System.out.println("-------------"); //Top
 
@@ -174,48 +288,71 @@ public class Game {
     }
 
     /**
-     *
-     * used by the printlevel class
-     *
-     * @param i the field you want to acces ( 0 - 8 )
-     * @return the character that needs to be placed here
+     * Prints the start screen
      */
-    private String getCharacter(int i){
+    private void printIntro() {
 
-        if( level[i] == 0) return("" + (i + 1));
-        else if( level[i] == 1) return("X");
-        else if( level[i] == 2) return("O");
-        else return("E");
+        /*
+         _______ _         ╭╯╭╯╭╯    _______
+        |__   __(_)       ╱▔▔▔▔▔╲▔╲ |__   __|
+           | |   _  ___  ╱ ╭╮ ╭╮ ╲╮╲   | | ___   ___
+           | |  | |/ __| ▏ ▂▂▂▂▂ ▕╮▕   | |/ _ \ / _ \
+           | |  | | (__  ▏ ╲▂▂▂╱ ▕╮▕   | | (_) |  __/
+           |_|  |_|\___| ╲▂▂▂▂▂▂▂▂╲╱   |_|\___/ \___|
+         */
 
-    }
-
-    /**
-     * Set a field in the level
-     *
-     * @param value 0 (empty), 1 (x) or 2 (o)
-     * @param position the field of the level you want to set.
-     */
-    private void setField(int value, int position) {
-        position--;
-        level[position] = value;
-    }
-
-    private void clearLevel(){
-
-        for(int i = 0; i < 9; i++) {
-            level[i] = 0;
-        }
-
+        println(" _______ _         ╭╯╭╯╭╯    _______");
+        println("|__   __(_)       ╱▔▔▔▔▔╲▔╲ |__   __|");
+        println("   | |   _  ___  ╱ ╭╮ ╭╮ ╲╮╲   | | ___   ___");
+        println("   | |  | |/ __| ▏ ▂▂▂▂▂ ▕╮▕   | |/ _ \\ / _ \\");
+        println("   | |  | | (__  ▏ ╲▂▂▂╱ ▕╮▕   | | (_) |  __/");
+        println("   |_|  |_|\\___| ╲▂▂▂▂▂▂▂▂╲╱   |_|\\___/ \\ ___|");
+        println("");
+        println("TicTacoToe");
+        println("---------------------");
+        println("Created by Cor Pruijs");
+        println("");
+        println("Press enter to begin with two players, or type AI to play against the AI");
     }
 
     /**
      * Prints a lot of empty lines to create the illusion of an "empty" console
      */
-    private void clearScreen(){
+    private void clearScreen() {
 
         for(int i = 0; i < 100; i++) {
             System.out.println("");
         }
+    }
+
+
+
+
+
+    private void scanInput() {
+
+        String userInput;
+
+        try {
+        userInput = scanIn.nextLine();
+        lastUserInput = "" + userInput;
+
+        }
+        catch ( java.util.NoSuchElementException x) {
+            lastUserInput = "";
+
+        }
+
+    }
+
+    /**
+     * Checks if the lastUserInput is a valid field
+     */
+    private boolean lastUserInputIsAField() {
+        for (int i = 1; i <= 9; i++) {
+            if (lastUserInput.equals("" + i)) return true;
+        }
+        return false;
     }
 
     /**
@@ -240,68 +377,10 @@ public class Game {
     }
 
     /**
-     * Prints the start screen
+     * Checks if the game is a tie
+     * @return true if the game is a tie, false if it isn't
      */
-    private void printIntro(){
-
-        /*
-         _______ _         ╭╯╭╯╭╯    _______
-        |__   __(_)       ╱▔▔▔▔▔╲▔╲ |__   __|
-           | |   _  ___  ╱ ╭╮ ╭╮ ╲╮╲   | | ___   ___
-           | |  | |/ __| ▏ ▂▂▂▂▂ ▕╮▕   | |/ _ \ / _ \
-           | |  | | (__  ▏ ╲▂▂▂╱ ▕╮▕   | | (_) |  __/
-           |_|  |_|\___| ╲▂▂▂▂▂▂▂▂╲╱   |_|\___/ \___|
-         */
-
-        println(" _______ _         ╭╯╭╯╭╯    _______");
-        println("|__   __(_)       ╱▔▔▔▔▔╲▔╲ |__   __|");
-        println("   | |   _  ___  ╱ ╭╮ ╭╮ ╲╮╲   | | ___   ___");
-        println("   | |  | |/ __| ▏ ▂▂▂▂▂ ▕╮▕   | |/ _ \\ / _ \\");
-        println("   | |  | | (__  ▏ ╲▂▂▂╱ ▕╮▕   | | (_) |  __/");
-        println("   |_|  |_|\\___| ╲▂▂▂▂▂▂▂▂╲╱   |_|\\___/ \\ ___|");
-        println("");
-        println("TicTacoToe");
-        println("---------------------");
-        println("Created by Cor Pruijs");
-        println("");
-        println("Press enter to begin");
-    }
-
-    /**
-     * A shorter version of System.out.println() because this program uses a lot of console printing
-     * @param x the string to print
-     */
-    private void println(String x) {
-        System.out.println(x);
-    }
-
-
-    private void scanInput() {
-
-        String userInput;
-
-        try {
-        userInput = scanIn.nextLine();
-        lastUserInput = "" + userInput;
-
-        }
-        catch ( java.util.NoSuchElementException x) {
-            lastUserInput = "";
-
-        }
-
-    }
-    /**
-     * Checks if the lastUserInput is a field
-     */
-    private boolean lastUserInputIsAField() {
-        for (int i = 1; i <= 9; i++) {
-            if (lastUserInput.equals("" + i)) return true;
-        }
-        return false;
-    }
-
-    public boolean gameIsTie(){
+    private boolean gameIsTie(){
         for (int i = 0; i <= 8; i++) {
             if (level[i] == 0) return false;
         }
